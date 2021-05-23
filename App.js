51b6c0789db8b203ef13bcbd0037493f596601c8
Zeeshan Khan <zeeskhan1990@gmail.com>
2021-05-23 23:01:16 +0530

    Set of changes

--- App.js
+++ App.js
@@ -1,39 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity, PermissionsAndroid  } from 'react-native';
 import SmsListener from 'react-native-android-sms-listener';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-/* SmsListener.addListener(message => {
-  console.info(message)
-}) */
-
-async function getPermission() {
-  try {
-    await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.READ_SMS,
-      {
-        title: "(...)",
-        message: "Why you're asking for..."
-      }
-    );
-    await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.RECEIVE_SMS, {
-      title: 'Receive SMS',
-      message: 'Need access to receive sms, to verify OTP'
-    }
-    );
-  } catch (err) {}
-}
-
 const storeData = async (value) => {
   try {
-    await AsyncStorage.setItem('@storage_Key', value)
-    alert(`URL saved ${value}`)
+    const storingValue = value.replace(/\/+$/, "");
+    await AsyncStorage.setItem('@storage_Key', storingValue)
+    alert(`URL saved ${storingValue}`)
   } catch (e) {
     // saving error
     console.log("ERROR IN STORING DATA")
-    console.log(err)
+    console.log(e)
   }
 }
 
@@ -65,50 +44,79 @@ const showData = async () => {
   }
 }
 
+const sendToServer = async (data) => {
+  const requestUrl = await getData()
+  const message = typeof data !== 'string' ? JSON.stringify(data) : data
+  console.log("Sending this to server")
+  console.log(message)
+  console.log(`${requestUrl}/data`)
+  fetch(`${requestUrl}/data`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }, 
+    body: message
+  });
+}
 
-export default function App() {
-  const [hookUrl, setHookUrl] = useState('')
+async function getPermission() {
+  try {
+    const permToRead = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.READ_SMS,
+      {
+        title: "(...)",
+        message: "Why you're asking for..."
+      }
+    );
+    const permToReceive = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.RECEIVE_SMS, {
+      title: 'Receive SMS',
+      message: 'Need access to receive sms, to verify OTP'
+    }
+    );
+    return {permToRead, permToReceive}
+  } catch (err) {
+    alert("Failed to Get Permissions")
+    console.log(err)
+    sendToServer(err)
+  }
+}
 
-  async function listenSms() {
-    const resp = await getPermission();
-    const requestUrl = await getData()
-    console.log(resp);
-    const listenerSub = SmsListener.addListener(message => {
+async function listenSms() {
+  const resp = await getPermission();
+  console.log("PERMISSION RESPONSE")
+  console.log(resp);
+  //sendToServer(resp)
+  //const requestUrl = await getData()
+  const listenerSub = SmsListener.addListener(message => {
+    const executor = async (message) => {
+      console.log("***** New Message Received ********")
       console.log(message);
-
+      const requestUrl = await getData()
       if(message && message.originatingAddress === "AX-NHPSMS") {
+        console.log("OTP FOUND")
         const OtpMessageNumbers = (message.body.match(/\d+/g) || []).map(n => parseInt(n))
         const otpNumber = OtpMessageNumbers && OtpMessageNumbers.length > 0 && OtpMessageNumbers[0]
-        console.log("OTP FOUND")
         console.log(otpNumber)
         console.log(requestUrl)
-        console.log(`${requestUrl}/?source=cowin&otp=${otpNumber}`)
-        otpNumber && fetch(`${requestUrl}/?source=cowin&otp=${otpNumber}`)
+        console.log(`${requestUrl}/otp?source=cowin&otp=${otpNumber}`)
+        otpNumber && fetch(`${requestUrl}/otp?source=cowin&otp=${otpNumber}`)
+      } else {
+        console.log("NON OTP MESSAGE")
+        sendToServer(message)
       }
-
-      // fetch('https://young-sloth-54.loca.lt/hook', {
-      //   method: 'POST',
-      //   /* headers: {
-      //     Accept: 'application/json',
-      //     'Content-Type': 'application/json'
-      //   }, */
-      //   body: JSON.stringify(message)
-      // });
-      
-    });
-    return listenerSub
-  }
-
-  useEffect(() => {
-    let listenerSub = null
-    const setupSmsListener = async () => {
-      listenerSub = await listenSms()
     }
-    setupSmsListener()
-    return () => {
-      listenerSub.remove()
-    }
-  },[])
+    executor(message)
+  });
+  return listenerSub
+}
+
+listenSms()
+
+
+export default function App() {
+  const [hookUrl, setHookUrl] = useState('https://razor-webhook.loca.lt')
 
   return (
     <View style={styles.container}>
